fd728cc1a7e9aea92f33b3c0d9ebb034c056ff19
Ewann <ewannpv@gmail.com>
2021-09-17 09:16:12 +0200

    Removes import

--- lib/screens/dashboard/components/stats_card.dart
+++ lib/screens/dashboard/components/stats_card.dart
@@ -4,7 +4,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:malubullule/constants.dart';
 import 'package:malubullule/providers/drink_stats_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StatsCard extends StatelessWidget {
   const StatsCard({Key? key}) : super(key: key);
